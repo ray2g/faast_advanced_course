0e4c38ebe518af5068e2ae8cdc0632d4d8d2efda
ray2g <raimundojoo@gmail.com>
2024-01-09 18:05:11 +0000

    :package: NEW: NEW: backup

---
+++ assignments/assignment_3/life_expectancy_backup/tests/__init__.py
@@ -0,0 +1,8 @@
+"""Test suite for the 1st assignment"""
+from pathlib import Path
+
+
+PROJECT_DIR = Path(__file__).parents[2]
+PACKAGE_DIR = PROJECT_DIR / "life_expectancy"
+FIXTURES_DIR = PACKAGE_DIR / "tests" / "fixtures"
+OUTPUT_DIR = PACKAGE_DIR / "data"
---
+++ assignments/assignment_3/life_expectancy_backup/tests/test_cleaning.py
@@ -0,0 +1,21 @@
+"""Tests for the cleaning module"""
+from pathlib import Path
+import pandas as pd
+
+from life_expectancy_backup.cleaning import clean_data
+from . import OUTPUT_DIR
+
+
+def test_clean_data(pt_life_expectancy_expected):
+    """Run the `clean_data` function and compare the output to the expected output"""
+
+    input_path: Path = OUTPUT_DIR / "eu_life_expectancy_raw.tsv"
+    output_path: Path = OUTPUT_DIR / "pt_life_expectancy.csv"
+    
+    clean_data(input_path, output_path, "PT")
+    pt_life_expectancy_actual = pd.read_csv(
+        OUTPUT_DIR / "pt_life_expectancy.csv"
+    )
+    pd.testing.assert_frame_equal(
+        pt_life_expectancy_actual, pt_life_expectancy_expected
+    )
