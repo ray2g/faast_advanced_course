e6d46a11c8b9065c3b1d51355f1b01678cac6539
ray2g <raimundojoo@gmail.com>
2024-02-09 16:53:53 +0000

    :ok_hand: IMPROVE: Addition of a test for clean_data_json, and aplication of set_strategy method.

--- assignments/life_expectancy/tests/test_cleaning.py
+++ assignments/life_expectancy/tests/test_cleaning.py
@@ -2,7 +2,7 @@
 
 
 import pandas as pd
-from life_expectancy.cleaning import clean_data
+from life_expectancy.context import Context, ConcreteTsvStrategy, ConcreteJsonStrategy
 from life_expectancy.regions import Region
 from pandas import DataFrame
 
@@ -12,14 +12,32 @@ def test_clean_data(eu_life_expectancy_raw: DataFrame, eu_life_expectancy_expect
     """
     Tests the clean_data module comparing the output with an expected output.
 
-    :param eu_life_expectancy_raw: Sampled Dataframe.
+    :param eu_life_expectancy_raw: Tsv sampled Dataframe.
     :param eu_life_expectancy_expected: Expected DataFrame.
     """
-    eu_life_expectancy_obtained = clean_data(eu_life_expectancy_raw, Region.PT).reset_index(drop=True)
+    context = Context()
+    context.set_strategy(ConcreteTsvStrategy())
+    eu_life_expectancy_obtained = context._strategy.cleaner(eu_life_expectancy_raw, Region.PT).reset_index(drop=True)
     
     pd.testing.assert_frame_equal(
                         eu_life_expectancy_obtained,
                         eu_life_expectancy_expected
     )
 
+def test_clean_data_json(eu_life_expect_json: DataFrame, pt_life_expectancy_expected: DataFrame
+    ) -> None:
+    """
+    Tests the clean_data_json module comparing the output with an expected output.
+
+    :param eu_life_expect_json: Json Dataframe.
+    :param pt_life_expectancy_expected: Expected cleaned DataFrame.
+    """
+    context = Context()
+    context.set_strategy(ConcreteJsonStrategy())
+    eu_life_expectancy_obtained = context._strategy.cleaner(eu_life_expect_json, Region.PT).reset_index(drop=True)
+    
+    pd.testing.assert_frame_equal(
+                        eu_life_expectancy_obtained,
+                        pt_life_expectancy_expected
+    )
 
